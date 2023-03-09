5bfa6b6f8266e2ff750734bfdce187a6626cf4e3
Alex <sorath2@yandex.ru>
2023-03-09 13:32:00 +0500

    ФЗ Спринт6 v.1.7

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -84,13 +84,15 @@ class Follow(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='following',
-        verbose_name='Автор'
+        verbose_name='Автор',
+        null=True
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='follower',
-        verbose_name='Подписчик'
+        verbose_name='Подписчик',
+        null=True
     )
 
     class Meta:
