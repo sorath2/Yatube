e3d49f55ed1a8c4922955187d96537ee20d37066
Alex <sorath2@yandex.ru>
2023-03-09 13:10:42 +0500

    ФЗ Спринт6 v.1.5

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -97,7 +97,7 @@ class Follow(models.Model):
         ordering = ['-author']
         constraints = [
             models.UniqueConstraint(fields=['author', 'user'],
-                                    name='follow constraint')
+                                    name='unique_members')
         ]
 
     def __str__(self):
