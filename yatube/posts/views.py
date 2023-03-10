0a3fbeca8b9ac13e16040df1e51609688cfd97e3
Alex <sorath2@yandex.ru>
2023-03-10 13:51:30 +0500

    ФЗ Спринт6 v.1.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -138,7 +138,8 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     user = get_object_or_404(User, username=username)
-    if user != request.user:
+    author = get_object_or_404(User, username=request.user)
+    if user != request.user and (user.id != author.id):
         Follow.objects.create(author=user, user=request.user)
     return render(request, 'posts/index.html')
 
