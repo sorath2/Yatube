5a2cc830847572e9fa25b796cdcd0904d4b131c7
Alex <sorath2@yandex.ru>
2023-03-10 10:14:36 +0500

    ФЗ Спринт6 v.1.7.12

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -85,18 +85,15 @@ class Follow(models.Model):
         on_delete=models.CASCADE,
         related_name='follower',
         verbose_name='Подписчик',
-        null=False,
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='following',
         verbose_name='Автор',
-        null=False,
     )
 
     class Meta:
-        ordering = ['-author']
         constraints = [
             models.UniqueConstraint(fields=['user', 'author'],
                                     name='unique_follow')
